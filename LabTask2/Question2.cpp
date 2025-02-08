efc82f7582f81f0d2d9f48570bd7811b1f24d8be
Aly <aly.kazani52@gmail.com>
2025-02-08 18:33:52 +0500

    Update Question2.cpp
    Git Issue
    ```
    void* Inc_Array(void* a, int size){
    	int* arr = static_cast<int*>(a);
    	for(int i = 0 ; i < size;i++){
    		arr[i]++;
    	}
    	return static_cast<void*>(arr);
    }
    ```
    
    Fixed.
    The function will now return a void reference.

--- LabTask2/Question2.cpp
+++ LabTask2/Question2.cpp
@@ -6,20 +6,32 @@ void* Inc_Array(void* a, int size){
 	for(int i = 0 ; i < size;i++){
 		arr[i]++;
 	}
-	return arr;
+	return static_cast<void*>(arr);
 }
 
 int main(int argc, char** argv){
+	
 	int* arr = new int[argc-1];
+	
 	for(int i = 0 ; i < argc-1; i++){
 		arr[i] = atoi(argv[i+1]);
 	}
+	
 //	current arr
-	void* newarr = Inc_Array(static_cast<void*>(arr), argc-1);
-	int* result = static_cast<int*>(arr);
+	for(int k = 0 ; k < argc-1 ; k++){
+		cout<<arr[k]<<" ";
+	}
+	cout<<endl;
+	
+	//cast as void in parameter
+	Inc_Array(static_cast<void*>(arr), argc-1);
+	
+	//cast back to int
+	static_cast<int*>(arr);
+	
 //	Result
 	for(int j = 0 ; j < argc-1 ; j++){
-		cout<<result[j]<<" ";
+		cout<<arr[j]<<" ";
 	}
 	
 	delete[] arr;
